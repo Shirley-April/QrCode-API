f7a9ea8e1f546d8c68ab99b2e2cee15641401678
unknown <shirley@ipayafrica.com>
2023-02-05 08:26:48 +0300

    sending response as binary data

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const { response } = require("express")
 const express = require("express")
+var cors = require('cors')
+
+const Qrcode = require("qrcode")
+
+const fs = require("fs")
 
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send("Hello there!")
+app.use(cors())
+
+app.get("/qrcode/binary", (req, res) => {
+    Qrcode.toFile("image.png", "New Three Binary Data", (err) => {
+        if(err) {
+            return res.status(500).send({ error: 'Error reading file' });
+        }
+    })
+    fs.readFile('./image.png', (err, data) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send({ error: 'Error reading file' });
+        }
+        res.send(data);
+      });
 })
 
 const port = 8000
