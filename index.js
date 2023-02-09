e5ba97811d84438c737e0553767c13b918ae93d6
unknown <shirley@ipayafrica.com>
2023-02-09 13:31:25 +0300

    return file as binary data

--- index.js
+++ index.js
@@ -3,41 +3,22 @@ var cors = require("cors");
 
 const Qrcode = require("qrcode");
 
-const fs = require("fs");
-
 const app = express();
 
 app.use(cors());
 
-app.get("/qrcode/binary", (req, res) => {
-  // Qrcode.toFile("image.png", "New Five Binary Data", (err) => {
-  //     if(err) {
-  //         return res.status(500).send({ error: 'Error reading file' });
-  //     }
-  //     fs.readFile('./image.png', (err, data) => {
-  //         if (err) {
-  //           console.error(err);
-  //           return res.status(500).send({ error: 'Error reading file' });
-  //         }
-  //         res.send(data);
-  //       });
-  // })
+app.get("/qrcode", (req, res) => {
+  const text = "New Text";
 
-  Qrcode.toFile("image.png", "New Eleven Binary Data")
-    .then(() => {
-      fs.readFile("./image.png", (err, data) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).send({ error: "Error reading file" });
-        }
-        res.send(data);
-      });
-    })
-    .catch((err) => {
+  Qrcode.toFile("image.png", text, (err) => {
+    if (err) {
       console.error(err);
-      return res.status(500).send({ error: "Error reading file" });
-    });
-}); 
+      return res.status(500).send({ error: "Error previewing image" });
+    }
+
+    res.sendFile('image.png' , { root : __dirname});
+  });
+});
 
 const port = 8000;
 app.listen(port, () => console.log(`App running on PORT ${port}`));
