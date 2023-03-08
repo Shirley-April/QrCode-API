c187c4cf821d0cb77b699d90c6081c9b748a2774
unknown <shirley@ipayafrica.com>
2023-03-08 18:04:17 +0300

    Encode body when recieved as object

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 var cors = require("cors");
+const bodyParser = require("body-parser")
 
 const Qrcode = require("qrcode");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json())
 
-app.get("/qrcode", (req, res) => {
-  const text = "New Text";
+app.post("/qrcode", (req, res) => {
+  const text = req.body.qrcode_text
 
-  Qrcode.toFile("image.png", text, (err) => {
+  let value
+
+  typeof text === "object" ? value = Object.values(text).join("\n") : value = text
+
+
+  Qrcode.toFile("image.png", value, (err) => {
     if (err) {
       console.error(err);
       return res.status(500).send({ error: "Error previewing image" });
@@ -19,6 +26,31 @@ app.get("/qrcode", (req, res) => {
     res.sendFile('image.png' , { root : __dirname});
   });
 });
+
+
+// app.post("/qrcode/2", (req, res) => {
+//   // const text = req.body.qrcode_text
+//   const text = req.body.qrcode_text
+
+//   Qrcode.toDataURL(text, (err, url) => {
+//     if (err) {
+//       console.error(err);
+//       return res.status(500).send({ error: "Error previewing image" });
+//     }
+
+//     res.send(url)
+//   })
+
+//   // Qrcode.toFile("image.png", text, (err) => {
+//   //   if (err) {
+//   //     console.error(err);
+//   //     return res.status(500).send({ error: "Error previewing image" });
+//   //   }
+
+//   //   res.sendFile('image.png' , { root : __dirname});
+//   // });
+// });
+
 
 const port = 8000;
 app.listen(port, () => console.log(`App running on PORT ${port}`));
